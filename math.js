42e912ce295dbc74c21cc46651e8280dbbedce5f
masudulalam <masuudulalam@gmail.com>
2024-11-15 04:31:24 +0600

    added math.js

---
+++ math.js
@@ -0,0 +1,43 @@
+// Math.min
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const minNumber = Math.min(2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log(minNumber); 
+
+// Math.max
+const maxNumber = Math.max(2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log(maxNumber);
+
+
+// To get Pi Value ---> Math.PI
+console.log(Math.PI);
+
+
+// To get absolute value: Math.abs
+console.log(Math.abs(-23));
+console.log(Math.abs(23));
+console.log(5-10);
+console.log(Math.abs(5-10));
+
+// Math.round
+console.log(Math.round(5.7));
+console.log(Math.round(10.4));
+console.log(Math.round(231.5));
+
+// Math.floor
+console.log(Math.floor(231.5));
+console.log(Math.floor(231.95));
+
+console.log('----------------------');
+
+// Math.ceil
+console.log(Math.ceil(231.5));
+console.log(Math.ceil(231.1)); 
+
+console.log('----------------------');
+
+// Math.random
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.round(Math.random()*100));
+
