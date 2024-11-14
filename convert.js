e3d793ecefe7e98050ff57ff7bb1724c3e812372
masudulalam <masuudulalam@gmail.com>
2024-11-15 01:20:53 +0600

    added inch to feet converter

---
+++ convert.js
@@ -0,0 +1,24 @@
+// Inch to Feet
+function inchToFeet (inch) {
+    const feet = inch / 12;
+
+    return feet;
+}
+
+// const output = inchToFeet(70.5);
+
+// console.log(output, 'Feet');
+
+// output feet and inch
+function inchToFeet2 (inch) {
+    const feetFraction = inch / 12;
+    const feetInt = parseInt(feetFraction);
+    const inchRemaining = inch % 12;
+    const result = feetInt + ' feet' + ' ' + inchRemaining + ' Inch';
+
+    return result;
+}
+
+const output = inchToFeet2(70.5);
+
+console.log(output);
